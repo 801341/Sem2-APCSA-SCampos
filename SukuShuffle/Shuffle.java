1ed2b3a4354905b7ca4afd40afa7d2d635aec654
Stephanie Campos <scampos@WS-ACADBUS.EDU>
2020-02-04 10:29:55 -0800

    hi

---
+++ SukuShuffle/Shuffle.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Write a description of class shuffle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Shuffle
+{
+    public Shuffle(int[] s)
+{
+      int n =(int)(math.random()*52 + 1); 
+      
+}
+    
+    public static void main()
+    {
+        int[] nums = new int[52];
+        for(int i = 0; i < 52; i++){
+            nums[i] = i +
+    
+    Shuffle Shuff = new Shuffle(nums);
+  }
+}
